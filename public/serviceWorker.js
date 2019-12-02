349b0073469f9d653a2f117434ee7935b75b198e
Matthieu <matthieu.mauny@gmail.com>
2019-12-02 19:12:01 +0100

    Cache first

--- public/serviceWorker.js
+++ public/serviceWorker.js
@@ -1,11 +1,36 @@
+const CACHE_NAME = 'pwa-message-cache-v1';
+const urlsToCache = [
+    '/',
+    'index.html',
+    'manifest.json',
+    'logo.svg',
+    'favicon.ico',
+    'logo192.png'
+];
+
+const loadAssetManifest = async function () {
+    const res = await fetch("/asset-manifest.json");
+    return res.json();
+};
+
 self.addEventListener('install', (event) => {
     console.log('👷', 'install', event);
+    event.waitUntil(
+        loadAssetManifest().then(assetManifest =>
+            caches.open(CACHE_NAME).then((cache) => cache.addAll([...urlsToCache, ...assetManifest.entrypoints]))
+        )
+    )
 });
 
 self.addEventListener('activate', (event) => {
     console.log('👷', 'activate', event);
 });
 
-self.addEventListener('fetch', function(event) {
+self.addEventListener('fetch', function (event) {
     console.log('👷', 'fetch', event);
+    event.respondWith(
+        caches.match(event.request).then(function (response) {
+            return response || fetch(event.request);
+        })
+    );
 });
