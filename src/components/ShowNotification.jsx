669743f89df6b9689bd0d7a4f35f8b80360d9f40
Matthieu <matthieu.mauny@gmail.com>
2019-12-02 22:47:34 +0100

    Notification with options

--- src/components/ShowNotification.jsx
+++ src/components/ShowNotification.jsx
@@ -5,7 +5,15 @@ function ShowNotification() {
     const clicked = () => {
         if (Notification.permission === 'granted') {
             navigator.serviceWorker.getRegistration().then(reg => {
-                reg.showNotification('Hello !');
+                const options = {
+                    body: 'Une notification avec option !',
+                    icon: 'logo192.png',
+                    vibrate: [100, 50, 100],
+                    date: {
+                        id: 'HELLO_NOTIF'
+                    }
+                };
+                reg.showNotification('Hello !', options);
             });
         }
     };
