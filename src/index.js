e1f6fda15d57f482ac974a2d9c3079794088e374
Matthieu <matthieu.mauny@gmail.com>
2019-12-03 11:45:13 +0100

    Subscribe to the push manager

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import {urlB64ToUint8Array} from "./services/PushService";
 
 ReactDOM.render(<App/>, document.getElementById('root'));
 
@@ -9,6 +10,23 @@ navigator.serviceWorker.register('serviceWorker.js');
 
 Notification.requestPermission().then(status => {
     console.log('Notification permission status:', status);
+});
+
+navigator.serviceWorker.getRegistration().then(async reg => {
+    try {
+        const subscription = await reg.pushManager.subscribe({
+            userVisibleOnly: true,
+            applicationServerKey: urlB64ToUint8Array('YOUR_PUBLIC_KEY')
+        });
+        console.log('User is subscribed:', JSON.stringify(subscription));
+
+    } catch (err) {
+        if (Notification.permission === 'denied') {
+            console.warn('Permission for notifications was denied');
+        } else {
+            console.error('Failed to subscribe the user: ', err);
+        }
+    }
 });
 
 
