4a9c8e4ed52a209d08a25072dddb0c4a7be5b7d6
Damien <damien.fauveau@outlook.com>
2019-12-05 11:58:58 +0100

    App : Added Menu

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
 import React from 'react';
+import {Switch, Route, BrowserRouter as Router} from 'react-router-dom';
 
+import Menu from './components/Menu';
+import Post from './components/Post';
+import List from './components/List';
 
 import './App.css';
 
 function App() {
     return (
-        <div className={'App'}>
-            A remplir
-        </div>
+        <Router>
+            <div className={'App'}>
+                <Menu/>
+                <Switch>
+                    <Route path={'/'}>
+                        <List/>
+                    </Route>
+                    <Route path={'/post'}>
+                        <Post/>
+                    </Route>
+                </Switch>
+            </div>
+        </Router>
     );
 }
 
